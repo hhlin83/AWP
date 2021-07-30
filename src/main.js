c3e88961b593cb99b5557713aba87f2648967bf5
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-07-30 00:56:05 -0700

    Render a 3D model in the scene

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // Three things required to display anything w/ three.js: scene, camera & renderer
 const scene = new THREE.Scene();
@@ -21,22 +22,22 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight, false);
 
 // Add a cube to scene
-const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-const cubeMaterial = new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
-  roughness: 0,
-  metalness: 0.9,
-});
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube); // added at coordinate (0, 0, 0)
+// const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+// const cubeMaterial = new THREE.MeshStandardMaterial({
+//   color: 0x00ff00,
+//   roughness: 0,
+//   metalness: 0.9,
+// });
+// const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// scene.add(cube); // added at coordinate (0, 0, 0)
 
 // Add texture to a cube
-// const cubeTexture = new THREE.TextureLoader().load('/img/baby-yoda.png');
-// const cube = new THREE.Mesh(
-//   new THREE.BoxGeometry(2, 2, 2),
-//   new THREE.MeshBasicMaterial({ map: cubeTexture })
-// );
-// scene.add(cube);
+const cubeTexture = new THREE.TextureLoader().load('/img/baby-yoda.png');
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshBasicMaterial({ map: cubeTexture })
+);
+scene.add(cube);
 
 // Add a line to scene
 const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
@@ -48,6 +49,20 @@ const points = [
 const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
 const line = new THREE.Line(lineGeometry, lineMaterial);
 scene.add(line);
+
+// Add a 3D model to scene
+const loader = new GLTFLoader();
+
+loader.load(
+  '/model/zelda_-_breath_of_the_wild/scene.gltf',
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 // Add lights to scene
 const pointLight = new THREE.PointLight(0xffffff, 10, 1000);
@@ -66,16 +81,16 @@ camera.position.set(0, 5, 10);
 camera.lookAt(0, 0, 0);
 
 // Add camera controls (triggered by mouse events)
-// const cameraControls = new OrbitControls(camera, renderer.domElement);
+const cameraControls = new OrbitControls(camera, renderer.domElement);
 
 // Add dragging controls over objects
-const dragControls = new DragControls([cube], camera, renderer.domElement);
-dragControls.addEventListener('dragstart', function (event) {
-  event.object.material.emissive.set(0xaaaaaa);
-});
-dragControls.addEventListener('dragend', function (event) {
-  event.object.material.emissive.set(0x000000);
-});
+// const dragControls = new DragControls([cube], camera, renderer.domElement);
+// dragControls.addEventListener('dragstart', function (event) {
+//   event.object.material.emissive.set(0xaaaaaa);
+// });
+// dragControls.addEventListener('dragend', function (event) {
+//   event.object.material.emissive.set(0x000000);
+// });
 
 // Render the scene
 function animate() {
@@ -86,7 +101,7 @@ function animate() {
   cube.rotation.y += 0.01;
 
   // update camera controls
-  // cameraControls.update();
+  cameraControls.update();
 
   renderer.render(scene, camera);
 }
